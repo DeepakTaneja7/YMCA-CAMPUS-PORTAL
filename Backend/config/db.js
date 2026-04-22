85da9a530399ce67679f579875e65b5b0b14fbb8
Deepak Taneja <deepaktaneja165@gmail.com>
2026-04-22 15:02:46 +0530

    fixed connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 
 // Create a connection pool (better for performance than a single connection)
 const pool = mysql.createPool({
-    uri: process.env.DATABASE_URL, 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    port: 12147,
     ssl: {
         rejectUnauthorized: false
     }
