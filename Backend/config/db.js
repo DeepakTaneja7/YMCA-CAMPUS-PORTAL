649de45c46069d0c7137bd169ff93a2e305f50f4
Deepak Taneja <deepaktaneja165@gmail.com>
2026-04-22 14:58:50 +0530

    changed the connection pool

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,16 +3,9 @@ require('dotenv').config();
 
 // Create a connection pool (better for performance than a single connection)
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    port: 12147, // Added your Aiven database port here
+    uri: process.env.DATABASE_URL, 
     ssl: {
-        rejectUnauthorized: false // Added SSL requirement for Aiven
+        rejectUnauthorized: false
     }
 });
 
